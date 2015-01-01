58fbb1c8f07e2f4e971b0276463370e87a059f43
Pawel Firlej <sq3ope@gmail.com>
2015-01-01 22:30:44 +0100

    Archetype generated

---
+++ dukes-age/src/main/java/firstcup/dukesage/resource/DukesAgeResource.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ *
+ * You may not modify, use, reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * http://java.net/projects/javaeetutorial/pages/BerkeleyLicense
+ */
+package firstcup.dukesage.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * REST Web Service
+ */
+@Path("dukesAge")
+public class DukesAgeResource {
+
+    /**
+     * Creates a new instance of DukesAgeResource
+     */
+    public DukesAgeResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of DukesAgeResource
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("text/plain")
+    public String getText() {
+        // Add code here
+    }
+}
---
+++ generate-dukes-age.sh
@@ -0,0 +1,4 @@
+mvn archetype:generate \
+-DarchetypeGroupId=org.glassfish.javaeetutorial.firstcup \
+-DarchetypeArtifactId=dukes-age-archetype \
+-DarchetypeVersion=RELEASE
